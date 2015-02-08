83326014dba87c6be24ffb9d08d85463a01ff8da
ExPHAT <me@exphat.com>
2015-02-08 14:19:49 -0500

    update
    fixing colorscheme
    
    maybe this will fix it?
    
    fixed

--- setup.sh
+++ setup.sh
@@ -3,9 +3,14 @@ apt-get install git
 
 # Setup Vundle (vim package manager)
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/terryma/vim-multiple-cursors ~/.vim/bundle/vim-multiple-cursors
+git clone https://github.com/tomasr/molokai.git ./molokai
+
+# Move/Remove molokai repo
+mv molokai/colors ~/.vim/colors
+rm -rf molokai
 
 # Move everything to home folder
-cp -r .vim ~/
 cp .vimrc ~/.vimrc
 cp .bash_profile ~/.bash_profile
 
